2aa3fe569fcafab066f8409094e9f5d1ab22edd0
Lenore Kubie <lenorekubie@albertinvent.com>
2024-10-25 14:43:39 -0600

    remove dead code

--- src/albert/collections/data_templates.py
+++ src/albert/collections/data_templates.py
@@ -1,5 +1,3 @@
-from collections.abc import Generator, Iterator
-
 from albert.collections.base import BaseCollection, OrderBy
 from albert.resources.data_templates import DataTemplate
 from albert.session import AlbertSession
@@ -14,36 +12,6 @@ class DataTemplateCollection(BaseCollection):
     def __init__(self, *, session: AlbertSession):
         super().__init__(session=session)
         self.base_path = f"/api/{DataTemplateCollection._api_version}/datatemplates"
-
-    # def _list_generator(
-    #     self,
-    #     *,
-    #     limit: int = 25,
-    #     offset: int | None = None,
-    #     name: str | None = None,
-    #     order_by: OrderBy = OrderBy.DESCENDING,
-    # ) -> Generator[DataTemplate, None, None]:
-    #     """NOTE: WE could add more allowed filters here, but for now, this is all we need."""
-    #     params = {
-    #         "sortBy": "createdAt",
-    #         "order": order_by.value,
-    #         "limit": str(limit),
-    #     }
-    #     if offset:  # pragma: no cover
-    #         params["offset"] = offset
-    #     if name:
-    #         params["text"] = name
-    #     while True:
-    #         response = self.session.get(self.base_path + "/search", params=params)
-    #         response_data = response.json()
-
-    #         raw_dts = response_data.get("Items", [])
-    #         start_offset = response_data.get("offset")
-    #         params["offset"] = int(start_offset) + int(limit)
-    #         for item in raw_dts:
-    #             yield DataTemplate(**item)
-    #         if not raw_dts or raw_dts == [] or len(raw_dts) < limit:
-    #             break
 
     def list(
         self,
