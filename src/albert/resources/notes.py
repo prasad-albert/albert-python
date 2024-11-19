7230380dca4432fe793f14eb4d629025f015d7e6
Lenore <KMnO4aq@gmail.com>
2024-11-19 09:14:14 -0700

    Update src/albert/resources/notes.py
    Co-authored-by: Sean Friedowitz <sean@albertinvent.com>

--- src/albert/resources/notes.py
+++ src/albert/resources/notes.py
@@ -5,6 +5,6 @@ from albert.resources.base import BaseEntityLink, BaseResource
 
 class Note(BaseResource):
     parent_id: str = Field(..., alias="parentId")
-    note: str = Field(...)
+    note: str
     id: str | None = Field(default=None, alias="albertId")
     attachments: list[BaseEntityLink] | None = Field(default=None, exclude=True, frozen=True)
