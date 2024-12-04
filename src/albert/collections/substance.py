8387162edf4839c805a84a0d2fde16b35f0ab7ba
Jonathan Welch <Jonathan@albertinvent.com>
2024-12-04 09:40:23 -0500

    updated docstrings

--- src/albert/collections/substance.py
+++ src/albert/collections/substance.py
@@ -5,30 +5,24 @@ from albert.session import AlbertSession
 
 class SubstanceCollection(BaseCollection):
     """
-    SubstanceCollection is a collection class for managing substances.
+    SubstanceCollection is a class for managing substances within the Albert framework.
 
     Parameters
     ----------
-    session : Albert
-        The Albert session instance.
+    session : AlbertSession
+        An instance of the Albert session used for API interactions.
 
     Attributes
     ----------
     base_path : str
-        The base URL for substance API requests.
+        The base URL for making API requests related to substances.
 
     Methods
     -------
-    create(substance: Substance) -> Substance
-        Creates a new substance.
-    get_by_id(substance_id: str) -> Optional[Substance]
-        Retrieves a substance by its ID.
-    update(substance_id: str, patch_data: dict) -> bool
-        Updates a substance by its ID.
-    delete(substance_id: str) -> bool
-        Deletes a substance by its ID.
-    list(limit: int, start_key: Optional[str]) -> Optional[List[Substance]]
-        Lists substances with optional filters.
+    get_by_ids(cas_ids: list[str], region: str = "US") -> list[SubstanceTypes]
+        Retrieves a list of substances by their CAS IDs and optional region.
+    get_by_id(cas_id: str, region: str = "US") -> SubstanceTypes
+        Retrieves a single substance by its CAS ID and optional region.
     """
 
     _api_version = "v3"
@@ -37,23 +31,23 @@ class SubstanceCollection(BaseCollection):
         super().__init__(session=session)
         self.base_path = f"/api/{SubstanceCollection._api_version}/substances"
 
-    def get_by_ids(self, cas_ids: list[str], region: str = "US") -> list[SubstanceTypes]:
+    def get_by_ids(self, *, cas_ids: list[str], region: str = "US") -> list[SubstanceTypes]:
         url = f"{self.base_path}"
         response = self.session.get(url, params={"casIDs": ",".join(cas_ids), "region": region})
         return SubstanceResponse.model_validate(response.json()).substances
 
-    def get_by_id(self, cas_id: str, region: str = "US") -> SubstanceTypes:
+    def get_by_id(self, *, cas_id: str, region: str = "US") -> SubstanceTypes:
         """
-        Get a substance by its ID.
+        Get a substance by its CAS ID.
 
         Parameters
         ----------
-        substance_id : str
-            The ID of the substance to retrieve.
+        cas_id : str
+            The CAS ID of the substance to retrieve.
 
         Returns
         -------
-        Substance | None
-            The retrieved substance or None if not found.
+        SubstanceTypes
+            The retrieved substance or raises an error if not found.
         """
         return self.get_by_ids(cas_ids=[cas_id], region=region)[0]
