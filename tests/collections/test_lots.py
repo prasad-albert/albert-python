435dbbc371d09cf8ad367a5e1c7473a59c03312d
Lenore Kubie <lenorekubie@albertinvent.com>
2024-12-09 13:52:56 -0700

    add method to get a list of Lots by id

--- tests/collections/test_lots.py
+++ tests/collections/test_lots.py
@@ -26,3 +26,11 @@ def test_get_by_id(client: Albert, seeded_lots: list[Lot]):
     got_lot = client.lots.get_by_id(id=seeded_lots[0].id)
     assert got_lot.id == seeded_lots[0].id
     assert got_lot.external_barcode_id == seeded_lots[0].external_barcode_id
+
+
+def test_get_by_ids(client: Albert, seeded_lots: list[Lot]):
+    got_lots = client.lots.get_by_ids(ids=[l.id for l in seeded_lots])
+    assert len(got_lots) == len(seeded_lots)
+    seeded_ids = [l.id for l in seeded_lots]
+    for l in got_lots:
+        assert l.id in seeded_ids
