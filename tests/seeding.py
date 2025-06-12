684733c5d0a1102b9958a26f579077c0c7f537dd
Sean Friedowitz <sean@albertinvent.com>
2025-06-12 14:52:34 -0700

    fix linter

--- tests/seeding.py
+++ tests/seeding.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from albert.resources.base import EntityLink, SecurityClass
 from albert.resources.btdataset import BTDataset
 from albert.resources.btinsight import BTInsight, BTInsightCategory
-from albert.resources.btmodel import BTModel, BTModelSessionCategory, BTModelSession, BTModelState
+from albert.resources.btmodel import BTModel, BTModelSession, BTModelSessionCategory, BTModelState
 from albert.resources.cas import Cas, CasCategory
 from albert.resources.companies import Company
 from albert.resources.custom_fields import (
